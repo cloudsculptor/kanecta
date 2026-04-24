3750f45b483ba4a5cff980543c07d85a80418cee
Richie Thomas <richardsempire@gmail.com>
2026-04-25 11:07:43 +1200

    Add cli

---
+++ kanecta-cli/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const datastoreRoot = path.resolve(__dirname, '../../kanecta-private-datastore');
+const cliConfig = JSON.parse(fs.readFileSync(path.join(datastoreRoot, 'cli', 'config.json'), 'utf8'));
+const { id } = cliConfig;
+
+const outputPath = path.resolve(__dirname, '..', 'output.md');
+fs.writeFileSync(outputPath, id);
+
+console.log(`Wrote ${outputPath}`);
