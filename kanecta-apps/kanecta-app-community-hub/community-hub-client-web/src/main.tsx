b697d06cdc0b58e99ab48c498b40dd945ab30e2f
Richie Thomas <richardsempire@gmail.com>
2026-06-19 05:52:12 +1200

    fix(pwa): disable service worker registration on nonprod
    On any hostname other than featherston.co.nz the SW is not registered,
    and any existing registration is actively unregistered so that stale
    PWA installs pick up fresh assets on next load.

--- kanecta-apps/kanecta-app-community-hub/community-hub-client-web/src/main.tsx
+++ kanecta-apps/kanecta-app-community-hub/community-hub-client-web/src/main.tsx
@@ -7,7 +7,13 @@ import "./App.scss";
 import App from "./App.tsx";
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").catch(() => {});
+  if (window.location.hostname === "featherston.co.nz") {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  } else {
+    navigator.serviceWorker.getRegistrations().then((regs) => {
+      regs.forEach((r) => r.unregister());
+    });
+  }
 }
 
 createRoot(document.getElementById("root")!).render(
